d505605d3f1dafae95267b7cdb9b980d8a370b60
Cristian Suazo Jara <cristian.suazo.j@mail.pucv.cl>
2023-10-31 18:53:43 -0300

    Intercambio entre columnas

--- tarea.py
+++ tarea.py
@@ -56,17 +56,33 @@ def calcular_suma_distancias_ponderadas(longitudes, preferencias):
 
     return suma_distancias
 
-nombre_archivo = 'S3'  # Reemplaza con el nombre de tu archivo
+def obtener_nombre_columna(columna, columnas_intercambiadas):
+    return chr(65 + columnas_intercambiadas.index(columna))  # Convierte el número de columna a letra según el orden actual
+
+def intercambiar_columnas(preferencias, columnas_intercambiadas, columna1, columna2):
+    for i in range(len(preferencias)):
+        preferencias[i][columna1], preferencias[i][columna2] = preferencias[i][columna2], preferencias[i][columna1]
+    columnas_intercambiadas[columna1], columnas_intercambiadas[columna2] = columnas_intercambiadas[columna2], columnas_intercambiadas[columna1]
+
+nombre_archivo = 'S5'
 
 datos = leer_archivo(nombre_archivo)
 if datos:
     num_puestos, longitudes, preferencias = datos
 
-    puesto1 = 2
-    puesto2 = 5
+    columnas_intercambiadas = [chr(65 + i) for i in range(num_puestos)]
 
-    # distancia_ponderada = calcular_distancia_ponderada(longitudes, preferencias, puesto1, puesto2)
-    # print(f"La distancia ponderada entre el puesto {puesto1} y el puesto {puesto2} es {distancia_ponderada}")
-
+    nombre_columna = ''.join(columnas_intercambiadas)
     suma_distancias = calcular_suma_distancias_ponderadas(longitudes, preferencias)
-    print(f"La suma de las distancias ponderadas para todos los pares de puestos es {suma_distancias}")
+    print(f"x = {nombre_columna}, f(x) = {suma_distancias}")
+
+    intercambio = input("Introduce las letras de las columnas a intercambiar (por ejemplo, A,B): ")
+    columnas_a_intercambiar = intercambio.split(',')
+    if len(columnas_a_intercambiar) == 2:
+        columna1 = columnas_intercambiadas.index(columnas_a_intercambiar[0])
+        columna2 = columnas_intercambiadas.index(columnas_a_intercambiar[1])
+        if 0 <= columna1 < num_puestos and 0 <= columna2 < num_puestos:
+            intercambiar_columnas(preferencias, columnas_intercambiadas, columna1, columna2)
+            nueva_suma_distancias = calcular_suma_distancias_ponderadas(longitudes, preferencias)
+            nueva_nombre_columna = ''.join(columnas_intercambiadas)
+            print(f"x = {nueva_nombre_columna}, f(x) = {nueva_suma_distancias}")
