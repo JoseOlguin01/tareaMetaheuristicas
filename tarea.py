787986bf316559a367bb01e5cdb21019a94a52b3
Cristian Suazo Jara <cristian.suazo.j@mail.pucv.cl>
2023-11-26 16:16:47 -0300

    Soluciones iniciales ahora son aleatorias
    Además, se reemplazó test.py por tarea.py.

--- tarea.py
+++ tarea.py
@@ -1,30 +1,15 @@
 import random
 import math
+import time
 
-def leer_archivo(nombre_archivo):
-    try:
-        with open(nombre_archivo, 'r') as archivo:
-            # Leer el número de puestos
-            num_puestos = int(archivo.readline())
+# Leer los datos del archivo de texto y almacenarlos en variables
+with open("QAP_sko56_04_n.txt", "r") as file:
+    lines = file.readlines()
+    n = int(lines[0])
+    tamanos = list(map(int, lines[1].split(",")))
+    clientes = [list(map(int, line.split(","))) for line in lines[2:]]
 
-            # Leer las longitudes de los puestos
-            longitudes = list(map(int, archivo.readline().split(',')))
-
-            # Crear una matriz para almacenar las preferencias de compra
-            preferencias = []
-            for _ in range(num_puestos):
-                linea = list(map(int, archivo.readline().split(',')))
-                preferencias.append(linea)
-
-            return num_puestos, longitudes, preferencias
-
-    except FileNotFoundError:
-        print("El archivo no se encontró.")
-    except Exception as e:
-        print("Ocurrió un error al leer el archivo:", str(e))
-
-    return None
-
+# Función para calcular la distancia ponderada entre puestos
 def calcular_distancia_ponderada(longitudes, preferencias, puesto1, puesto2):
     if puesto1 == puesto2:
         return 0
@@ -39,7 +24,7 @@ def calcular_distancia_ponderada(longitudes, preferencias, puesto1, puesto2):
     distancia_total = 0
 
     for i in range(inicio + 1, fin):
-        distancia_total += longitudes[i - 1]
+        distancia_total += longitudes[i-1]
 
     if puesto1 < puesto2:
         distancia_total += longitudes[puesto1 - 1] / 2
@@ -49,94 +34,91 @@ def calcular_distancia_ponderada(longitudes, preferencias, puesto1, puesto2):
 
     return distancia_ponderada
 
-def calcular_suma_distancias_ponderadas(longitudes, preferencias):
-    num_puestos = len(longitudes)
+# Función para calcular la suma de las distancias ponderadas
+def calcular_suma_distancias_ponderadas(x_actual, clientes, n):
+    num_puestos = n
     suma_distancias = 0
 
     for puesto1 in range(1, num_puestos + 1):
         for puesto2 in range(puesto1 + 1, num_puestos + 1):
-            suma_distancias += calcular_distancia_ponderada(longitudes, preferencias, puesto1, puesto2)
+            suma_distancias += calcular_distancia_ponderada(x_actual, clientes, puesto1, puesto2)
 
     return suma_distancias
 
-def obtener_nombre_columna(columna, columnas_intercambiadas):
-    # return chr(65 + columnas_intercambiadas.index(columna)) 
-    return str(columnas_intercambiadas.index(columna) + 1)
+# Función para generar un vecino cambiando el orden de dos puestos y sus clientes
+def generar_vecino(x, tamanos, clientes):
+    i, j = random.sample(range(n), 2)
+    vecino_orden = x[:]
+    vecino_tamanos = tamanos[:]
+    vecino_clientes = [row[:] for row in clientes]  # Crear una copia de la lista de preferencias
+    vecino_orden[i], vecino_orden[j] = vecino_orden[j], vecino_orden[i]
+    vecino_tamanos[i], vecino_tamanos[j] = vecino_tamanos[j], vecino_tamanos[i]
+    vecino_clientes[i], vecino_clientes[j] = vecino_clientes[j], vecino_clientes[i]
+    return vecino_orden, vecino_tamanos, vecino_clientes
 
-def intercambiar_columnas(preferencias, columnas_intercambiadas, columna1, columna2):
-    for i in range(len(preferencias)):
-        preferencias[i][columna1], preferencias[i][columna2] = preferencias[i][columna2], preferencias[i][columna1]
-    columnas_intercambiadas[columna1], columnas_intercambiadas[columna2] = columnas_intercambiadas[columna2], columnas_intercambiadas[columna1]
+# Implementación del algoritmo Simulated Annealing
+def simulated_annealing(tamanos, clientes, n, Tmax, Tmin, enfriamiento):
+    start_time = time.time()  # Registra el tiempo de inicio
+    x_actual = list(range(n))  # Orden inicial de los puestos. # Orden de lectura.
+    random.shuffle(x_actual) # Solución inicial: ahora es aleatoria.
+    mejor_solucion = x_actual
+    energia_actual = calcular_suma_distancias_ponderadas(tamanos, clientes, n)
+    mejor_energia = energia_actual
+    iteraciones = 5000
 
-nombre_archivo = 'QAP_sko56_04_n.txt'
-
-datos = leer_archivo(nombre_archivo)
-if datos:
-    num_puestos, longitudes, preferencias = datos
-
-    columnas_intercambiadas = list(range(1, num_puestos + 1))  # Utiliza números en lugar de letras
-
-    nombre_columna = ' '.join([str(i) for i in columnas_intercambiadas])  # Utiliza números en lugar de letras
-    suma_distancias = calcular_suma_distancias_ponderadas(longitudes, preferencias)
-    print(f"x = {nombre_columna}, f(x) = {suma_distancias}")
-
-    intercambio = input("Introduce los números de las columnas a intercambiar (por ejemplo, 1,2): ")
-    columnas_a_intercambiar = list(map(int, intercambio.split(',')))
-    if len(columnas_a_intercambiar) == 2:
-        columna1 = columnas_a_intercambiar[0]
-        columna2 = columnas_a_intercambiar[1]
-        if 1 <= columna1 <= num_puestos and 1 <= columna2 <= num_puestos:
-            intercambiar_columnas(preferencias, columnas_intercambiadas, columna1 - 1, columna2 - 1)  # Resta 1 para ajustar al índice
-            nueva_suma_distancias = calcular_suma_distancias_ponderadas(longitudes, preferencias)
-            nueva_nombre_columna = ' '.join([str(i) for i in columnas_intercambiadas])  # Utiliza números en lugar de letras
-            print(f"x = {nueva_nombre_columna}, f(x) = {nueva_suma_distancias}")
-
-def simulated_annealing_metropolis(longitudes, preferencias, Tmax, Tmin, enfriamiento, iteraciones):
-    num_puestos = len(longitudes)
-    mejor_orden = list(range(1, num_puestos + 1))  # Cambiado para iniciar desde 1 en lugar de 0
-    mejor_suma_distancias = calcular_suma_distancias_ponderadas(longitudes, preferencias)
-    mejor_nombre_columna = ''.join([str(i) for i in range(1, num_puestos + 1)])  # Utiliza números en lugar de letras
-
-    orden_actual = list(range(1, num_puestos + 1))  # Cambiado para iniciar desde 1 en lugar de 0
-    suma_distancias_actual = mejor_suma_distancias
-    nombre_columna_actual = mejor_nombre_columna
-
+    # Impresión de la solución inicial y su evaluación en la función objetivo con indices i (partiendo de i = 0)
+    print("Solución inicial (x):", [i for i in x_actual])
+    print("Energía inicial (f(x)):", energia_actual, '\n')
+    
     for i in range(iteraciones):
-        T = Tmax - i * ((Tmax - Tmin) / iteraciones)
+        
+        # Temperatura de inicio
+        T = Tmax
 
-        columna1, columna2 = random.sample(range(1, num_puestos + 1), 2)  # Utiliza números en lugar de letras
-        intercambiar_columnas(preferencias, orden_actual, columna1 - 1, columna2 - 1)  # Resta 1 para ajustar al índice
-        nueva_suma_distancias = calcular_suma_distancias_ponderadas(longitudes, preferencias)
-        nueva_nombre_columna = ' '.join([str(i) for i in orden_actual])  # Utiliza números en lugar de letras
+        # Generación de una nueva solución vecina
+        vecino_orden, vecino_tamanos, vecino_clientes = generar_vecino(x_actual, tamanos, clientes)
+        energia_vecino = calcular_suma_distancias_ponderadas(vecino_tamanos, vecino_clientes, n)
 
-        # Calcular la probabilidad de aceptación usando el criterio de Metrópolis con log-sum-exp trick
-        exponente = min(0, (suma_distancias_actual - nueva_suma_distancias) / T)
-        probabilidad_aceptacion = 1 / (1 + math.exp(exponente))
+        # Actualización de la solución
+        if energia_vecino < energia_actual:
+            x_actual = vecino_orden
+            tamanos = vecino_tamanos
+            clientes = vecino_clientes
+            energia_actual = energia_vecino
+            mejor_solucion = x_actual
+            mejor_energia = energia_actual
+        else:
+            if T >= Tmin and random.random() < math.exp((energia_actual - energia_vecino) / T):
+                x_actual = vecino_orden
+                tamanos = vecino_tamanos
+                clientes = vecino_clientes
+                energia_actual = energia_vecino
+                mejor_solucion = x_actual
+                mejor_energia = energia_actual
 
-        # Aceptar la nueva solución con Metrópolis criteria
-        if nueva_suma_distancias < suma_distancias_actual or random.random() < probabilidad_aceptacion:
-            suma_distancias_actual = nueva_suma_distancias
-            nombre_columna_actual = nueva_nombre_columna
+        # Aplicando enfriamiento
+        Tmax *= enfriamiento
 
-            # Actualizar la mejor solución si es necesario
-            if suma_distancias_actual < mejor_suma_distancias:
-                mejor_suma_distancias = suma_distancias_actual
-                mejor_orden = orden_actual[:]
-                mejor_nombre_columna = nombre_columna_actual
+    end_time = time.time()  # Registra el tiempo de finalización
+    elapsed_time = end_time - start_time  # Calcula el tiempo transcurrido
 
-        # Reducir la temperatura
-        # Tmax *= enfriamiento
+    # Impresión del tiempo medio de ejecución en milisegundos, aproximado a cuatro decimales
+    tiempo_medio_ms = round((elapsed_time) * 1000, 4)
 
-    return mejor_orden, mejor_nombre_columna, mejor_suma_distancias
+    return mejor_solucion, mejor_energia, tiempo_medio_ms
 
 # Parámetros del algoritmo
 Tmax = 1000
 Tmin = 1
-enfriamiento = 0.95
-iteraciones = 1000
+enfriamiento = 0.5
+for i in range(10):
+    print("----- EJECUCIÓN", i, ": -----")
+    # Ejecutar Simulated Annealing
+    mejor_orden, mejor_energia, tiempo_medio = simulated_annealing(tamanos, clientes, n, Tmax, Tmin, enfriamiento)
 
-# Ejecutar Simulated Annealing con el criterio de Metrópolis
-mejor_orden, mejor_nombre_columna, mejor_suma_distancias = simulated_annealing_metropolis(longitudes, preferencias, Tmax, Tmin, enfriamiento, iteraciones)
-
-print(f"Mejor orden de puestos: [{mejor_nombre_columna}]")
-print(f"Suma de distancias ponderadas de la mejor solución: {mejor_suma_distancias}")
+    # Métricas para análisis del código, comentado para entrega final
+    # Impresión de la mejor solución con indices i + 1
+    print("Mejor solución (x'):", [i for i in mejor_orden])
+    print("Energía final (f(x')):", mejor_energia)
+    print("Tiempo de ejecución (ms.):", tiempo_medio)
+    print("Tiempo de ejecución (seg.):", round(tiempo_medio / 1000,4), '\n')
